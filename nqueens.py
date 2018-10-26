e2928cd561520818eb003b4da8f67ea3b628dc5d
alex <uwalex@gmail.com>
2018-10-26 15:02:05 -0400

    Updated the is_col_valid functions, works much nicer now

--- nqueens.py
+++ nqueens.py
@@ -1,11 +1,23 @@
-   
+import itertools
+
 def share_diag(x0,x1,y0,y1):
     x_delta = abs(x0-y0)
     y_delta = abs(x1-y1)
     return x_delta == y_delta
 
-def next_row_clashes(partial_sol, next_row):
+def is_col_valid(partial_sol, col):
     """
+    Checks if the given col has any diag. violations with all previous
+    queens 0,1,2..col-1 columns
+    Assume: all values in partial_sol are unique
+
+    @param partial_sol[list]: A list representing a partial solution
+    @param col[int]: The column in question
+
+    @return True: if the column is valid
+    @return False: if there is a violation (diag)
+
+    ex.
       0 1 2 3 4 5 6 7
     0       Q            
     1                 
@@ -16,27 +28,49 @@ def next_row_clashes(partial_sol, next_row):
     6 Q               
     7                 
     = [6,4,2,0,5] = partial_sol
-    
-    Now we try and add another Queen to the next next_col = len(partial_sol)
-    Try and place the Queen on (x,y) = (next_col,next_row) and see if it shares
-    any diagonals with all previously places queens    
-    """
-    next_x = len(partial_sol)
-    next_y = next_row
-    for curr_x, curr_y in enumerate(partial_sol):
-        if share_diag(curr_x,curr_y, next_x, next_y):
-            return True
-    return False
+    Check col = 4 (the 5 above) => True    
+    """     
+    suspect_x = col
+    suspect_y = partial_sol[col]
+    for i in range(col):
+        curr_x = i
+        curr_y = partial_sol[i]
+        if share_diag(curr_x, curr_y, suspect_x, suspect_y):
+            return False
+    return True
 
 def is_board_valid(board_state):
     #Checks if there are any diagnoal conflicts 
     #Assuming that board_state is all unqiue numbers
     for curr_x in range(1,len(board_state)):
-        curr_y = board_state[curr_x]
-        if next_row_clashes(board_state[:curr_x],curr_y):            
+        if not is_col_valid(board_state, curr_x):
             return False
     return True
 
+def solve_N_queens(n):
+    """
+    Given n solve the N-queens problem
+    How to place N queens on an NxN grid 
+    without any of them sharing the same
+    vertical, horizontal, and diagonal space
+
+    @return: the solution
+    @return: the # of attempts it took to achieve that solution
+    """
+    #Init
+    attempts = 1 #The number of attempts it took to find a solution
+    solution = list(range(n))
+    permutations = itertools.permutations(solution)
+
+    # while True:
+    for potential_sol in permutations:
+        # print(list(potential_sol))
+        if is_board_valid(potential_sol):
+            return {
+                "solution":potential_sol,
+                "attempts":attempts
+            }
+        attempts+=1
 
 if __name__ == "__main__":
-    pass
+    print(solve_N_queens(8))
--- test_nqueens.py
+++ test_nqueens.py
@@ -9,14 +9,15 @@ class TestShareDiag(unittest.TestCase):
     def test2(self): self.assertEqual(nqueens.share_diag(5,2,4,1), True)
     def test3(self): self.assertEqual(nqueens.share_diag(5,2,4,3), True)
 
-class TestNextRowClashes(unittest.TestCase):
-    def test1(self): self.assertEqual(nqueens.next_row_clashes([6,4,2,0,5], 1), True) #Clash with 0
-    def test2(self): self.assertEqual(nqueens.next_row_clashes([6,4,2,0,5], 2), True) #Clash with 0
-    def test3(self): self.assertEqual(nqueens.next_row_clashes([6,4,2,0,5], 3), False)
-    def test4(self): self.assertEqual(nqueens.next_row_clashes([6,4,2,0,5], 4), True) #Clash with 5
-    def test5(self): self.assertEqual(nqueens.next_row_clashes([6,4,2,0,5], 6), True) #Clash with 5    
-    def test6(self): self.assertEqual(nqueens.next_row_clashes([6,4,2,0,5], 7), False) 
-    
+class TestIsColValid(unittest.TestCase):
+    def test1(self): self.assertEqual(nqueens.is_col_valid([6,4,2,0,5], 4), True)
+    def test2(self): self.assertEqual(nqueens.is_col_valid([2,0,1], 2), False) #Violation b/w 1 & 0
+    def test3(self): self.assertEqual(nqueens.is_col_valid([6,4,2,0,5,7,1,3], 7), True)
+    def test4(self): self.assertEqual(nqueens.is_col_valid([0,1], 1), False) #Violation b/w 1 & 0
+    def test5(self): self.assertEqual(nqueens.is_col_valid([5,6], 1), False) #Violation b/w 5 & 6
+    def test6(self): self.assertEqual(nqueens.is_col_valid([0,6,4,3], 3), False) #Violation b/w 4 & 3
+    def test7(self): self.assertEqual(nqueens.is_col_valid([0,6,4,5], 3), False) #Violation b/w 4 & 5
+    def test8(self): self.assertEqual(nqueens.is_col_valid([5,0,7], 2), False) #Violation b/w 5 & 7
 
 class TestIsBoardValid(unittest.TestCase):
     def test1(self): self.assertEqual(nqueens.is_board_valid([6,4,2,0,5,7,1,3]), True)
