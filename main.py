6d1ded2a715342f3fab48a803e928342baee575d
alex <uwalex@gmail.com>
2018-10-27 13:51:09 -0400

    Opps, removing refactoring junk

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from board import Board
 import random
 
-def solve_N_queens2(n):
+def solve_N_queens(n):
     """
     Given n solve the N-queens problem
     How to place N queens on an NxN grid 
@@ -27,19 +27,19 @@ def solve_N_queens2(n):
             }
         attempts+=1        
 
-def run_single_solution2(n):
+def run_single_solution(n):
     # Solve the N qeen problem and display the board and # attempts
     print("Running the {} Queen problem...".format(n))
-    solution = solve_N_queens2(n)
+    solution = solve_N_queens(n)
     board = solution["board"]
     attempts = solution["attempts"]
     print(board)
     print("Number of attempts made before solving = {}".format(attempts))
 
-def run_multiple_solutions2(highest_n):
+def run_multiple_solutions(highest_n):
     #Solve the N queen problem for everyting up to and including highest_n
     for curr_n in range(4,highest_n+1):
-        solution = solve_N_queens2(curr_n)
+        solution = solve_N_queens(curr_n)
         board = solution["board"]
         attempts = solution["attempts"]
         print("{:2}-Queen solved in {:8} attempts".format(curr_n, attempts))        
@@ -48,6 +48,6 @@ def test():
     pass
 
 if __name__ == "__main__":
-    run_single_solution2(8)
+    run_single_solution(8)
     print("------------------")
-    run_multiple_solutions2(10)
+    run_multiple_solutions(10)
